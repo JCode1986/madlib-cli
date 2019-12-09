efd780eb58e9af6c1af8c9723543136133e3d805
Joseph Hangarter <joseph.hangarter@yahoo.com>
2019-12-08 21:04:21 -0800

    Refactored code, all functionalities working, removed imported input: cannot open `input' (No such file or directory)

--- madlib.py
+++ madlib.py
@@ -1,59 +1,40 @@
-import fileinput
 import re
 
 #Madlib Game
 
-#write to file
-def write_file(path, contents):
-  with open(path, 'w') as f:
-    f.write(contents)
-
-#read template text
 def get_template_text(path):
     with open(path, 'r') as file:
         return(file.read())
 
-#get user's name; instruction on what to do
 def get_name():
     name = input('Hello there. What is your name? ')
-    print('Hello ' + name + ', let\'s play a game! I will ask you to type in certain types words. Hit enter after every word.')
+    print('Hello ' + name + ', let\'s play a game! I will ask you to type in certain types of words. Hit enter after every word.')
 
-#hard coded for now..
-word_type = ['adjective', 'adjective', 'first name', 'past tense verb', 'first name', 'adjective', 'adjective', 'plural noun', 'large animal', 'small animal', 'a girls name', 'adjective', 'plural noun', 'adjective', 'plural noun', 'number from 1 - 50', 'first name', 'number', 'plural', 'noun', 'number', 'plural noun']
+user_input = []
 
-#user input to replace strings with wrapped in {}
-user_input_list = []
+def prompts(string):
+    get_name()
+    word_type = []
+    word_type += re.findall(r"(?<={)[\w<>' -]+(?=})", string)
+    for word in word_type:
+        user_input.append(input('Enter ' + word + ': '))
+    return user_input
 
-#find {'string here'}
-def parse_template_text(template_text):
-    pass
+def new_string(lst, string):
+    remove_string_str_between_curlies = re.sub((r"(?<={)[\w<>' -]+(?=})"), '', string)
+    final_string = remove_string_str_between_curlies.format(*lst)
+    return final_string
 
-template = 'template.txt'
-get_template_text(template)
-template_string = get_template_text(template)
-
-def get_words():
-    for elem in word_type:
-        user_input = input('Enter ' + elem + ': ')
-        user_input_list.append(user_input)
-    print(user_input, user_input_list)
-
-def write_words(lst):
-    for elem in lst:
-        write_file('madlib.txt', elem)
-
-#regex replace words with {}
-with fileinput.FileInput('madlib.txt', inplace=True) as file:
-    for line in file:
-        line = re.sub(r"\{(.*?)\}", 'user inputs go here somehow', line)
-        print(line)
+def write_file_results(string, path, contents):
+  with open(path, 'w') as f:
+    f.write(string)
 
 def start():
-    get_name()
-    get_template_text(template)
-    get_words()
+    str_from_template = get_template_text('template.txt')
+    prompts(str_from_template)
+    final_string = new_string(user_input, str_from_template)
+    write_file_results(final_string, './madlib.txt', 'w')
+    print(final_string)
 
 if __name__ == '__main__':
     start()
-
-
---
+++ test_madlib.py
@@ -0,0 +1,3 @@
+from madlib import get_template_text, get_name, prompts, new_string, write_file_results
+import pytest
+
